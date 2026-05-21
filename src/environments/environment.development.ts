13bbcfa4c3eb37fb2b2a0d3ef36f731919618ed1
Fabrix265 <fabrix336@gmail.com>
2026-05-21 14:10:00 -0500

    Se cambio la Url a local para seguir con las pruebas

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://personally-wales-framing-men.trycloudflare.com',
+  apiUrl: 'http://localhost:8000',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://personally-wales-framing-men.trycloudflare.com',
+  apiUrl: 'http://localhost:8000',
 };
