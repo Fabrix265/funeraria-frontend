713f1ee607eb3b8a37f17b28d0f46d5177997517
Fabrix265 <fabrix336@gmail.com>
2026-05-22 04:38:25 -0500

    Se modificó la url del tunel para el modelo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://funeraria-inventario-inteligente.onrender.com',
-  iaApiUrl: 'https://tales-banners-signatures-rev.trycloudflare.com' 
+  iaApiUrl: 'https://seeing-belly-charms-connectors.trycloudflare.com' 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://funeraria-inventario-inteligente.onrender.com',
-  iaApiUrl: 'https://tales-banners-signatures-rev.trycloudflare.com'
+  iaApiUrl: 'https://seeing-belly-charms-connectors.trycloudflare.com'
 };
