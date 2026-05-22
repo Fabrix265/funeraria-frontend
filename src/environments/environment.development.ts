fdbcb0abf65f1045615514bbb779121b697edca9
Fabrix265 <fabrix336@gmail.com>
2026-05-22 04:29:05 -0500

    Se agrego la url del tunel para el modelo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://funeraria-inventario-inteligente.onrender.com',
-  iaApiUrl: 'http://137.184.63.223:8000' 
+  iaApiUrl: 'https://tales-banners-signatures-rev.trycloudflare.com' 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://funeraria-inventario-inteligente.onrender.com',
-  iaApiUrl: 'http://137.184.63.223:8000'
+  iaApiUrl: 'https://tales-banners-signatures-rev.trycloudflare.com'
 };
