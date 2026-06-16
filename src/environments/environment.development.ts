2cc5e9bab29d5eb14d9c06c74e9ce177054fb083
Fabrix265 <fabrix336@gmail.com>
2026-06-16 00:17:32 -0500

    Quité la api key jsjsjs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://funeraria-inventario-inteligente.onrender.com',
-  iaApiUrl: 'https://seeing-belly-charms-connectors.trycloudflare.com' 
+  apiUrl: 'https://funeraria-inventario-inteligente-wv7g.onrender.com',
+  iaApiUrl: 'https://novels-necklace-advocacy-hearing.trycloudflare.com',
+  stripePublicKey: 'pk_test_51Tdk6E2RTm0YmClc6mTm2QlWDa7WIdOqJd5HAgML3ZXmlvm13ZInFRpSBaw3P5k8Ut57ZEu8FUphafJ1gkpYET3800pIHpr0vC'
 };
